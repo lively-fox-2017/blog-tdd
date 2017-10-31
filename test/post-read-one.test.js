1e8997c80f26f90ceaf21181c0066d453e856c4b
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-10-31 16:06:39 +0700

    create test for read one post

---
+++ test/post-read-one.test.js
@@ -0,0 +1,142 @@
+'use strict'
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+
+const app = require('./../app');
+const models = require('./../models');
+
+chai.use(chaiHttp);
+
+describe('Read One Post: API: GET /post/:id', () => {
+  /*
+  Declare variable postId and author for testing
+  */
+  let validPostId = null;
+  let invalidPostId = require('mongoose').Types.ObjectId();
+  let validTitle = null;
+  let validText = null;
+  let validAuthor = null;
+  let validImageUrl = null;
+
+  /*
+  fake post
+  */
+  const fakePost = {
+    title: 'This Is A Post',
+    text: 'This is post content',
+    featured_image_url: 'https://www.codeproject.com/KB/GDI-plus/ImageProcessing2/flip.jpg'
+  }
+
+  /*
+  Before all tests, signup/create user to get jwtoken and create post for testing
+  */
+  before(done => {
+    chai.request(app)
+    .post('/signup')
+    .send({
+      username: 'user',
+      password: 'user'
+    })
+    .end((err, response) => {
+      let jwtoken = response.body.payload.jwtoken;
+      chai.request(app)
+      .post('/post')
+      .set('jwtoken', jwtoken)
+      .send(fakePost)
+      .end((err, response) => {
+        validPostId = response.body.payload._id;
+        validTitle = response.body.payload.title;
+        validText = response.body.payload.text;
+        validAuthor = response.body.payload.author.username;
+        validImageUrl = response.body.payload.featured_image_url;
+
+        done();
+      });
+    });
+  });
+
+  /*
+  After all tests, clear user and post database
+  */
+  after(done => {
+    models.User.remove({}, (err) => {
+      models.Post.remove({}, (err) => {
+        done();
+      });
+    });
+  });
+
+  /*
+  Test GET /post/:id response object
+  */
+  it('should generate a generic application response {status, message, payload, err}', (done) => {
+    chai.request(app)
+    .get('/post/' + validPostId)
+    .end((err, response) => {
+      const appResponse = response.body;
+
+      should.exist(appResponse);
+
+      appResponse.should.be.an('object');
+      appResponse.should.have.property('status');
+      appResponse.should.have.property('message');
+      appResponse.should.have.property('payload');
+      appResponse.should.have.property('err');
+
+      appResponse.status.should.be.a('number');
+      appResponse.message.should.be.a('string');
+      appResponse.payload.should.be.an('object');
+      should.not.exist(appResponse.err);
+
+      done();
+    });
+  });
+
+  /*
+  Test GET /post response payload
+  */
+  it('should contains a post', (done) => {
+    chai.request(app)
+    .get('/post/' + validPostId)
+    .end((err, response) => {
+      const appResponse = response.body;
+
+      response.status.should.equal(200);
+
+      appResponse.status.should.equal(200);
+      appResponse.message.should.be.a('string');
+      appResponse.payload.should.be.an('object');
+      should.not.exist(appResponse.err);
+
+      appResponse.payload.should.have.property('_id');
+      appResponse.payload.should.have.property('title');
+      appResponse.payload.should.have.property('text');
+      appResponse.payload.should.have.property('author');
+      appResponse.payload.should.have.property('featured_image_url');
+
+      appResponse.payload._id.should.equal(validPostId);
+      appResponse.payload.title.should.equal(validTitle);
+      appResponse.payload.text.should.equal(validText);
+      appResponse.payload.author.should.equal(validAuthor);
+      appResponse.payload.featured_image_url.should.equal(validImageUrl);
+
+      done();
+    });
+  });
+
+  /*
+  Test GET /post response payload with missing post id
+  */
+  it('should return a 406 error if post id is missing', (done) => {
+    chai.request(app)
+    .get('/post/')
+    .end((err, response) => {
+      should.exist(err);
+      response.status.should.equal(404);
+
+      done();
+    });
+  });
+});
