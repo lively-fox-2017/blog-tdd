6166dbd898e899b70693ffc00e1f24fbcff5bfc8
sisinduku <saptanto.sindu@gmail.com>
2017-11-08 21:19:03 +0700

    Edit Authentication

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+var FB = require('fb');
 
 class UserCtrl {
   static addUser(req, res, next) {
@@ -43,27 +44,38 @@ class UserCtrl {
   }
 
   static authUser(req, res, next) {
-    User.findOneAndUpdate({
-      userID: req.body.userID
-    }, {
-      lastLogin: new Date()
-    }, {
-      upsert: true,
-      setDefaultsOnInsert: true
-    }, function(err, user) {
-      if (err) {
-        console.error(err);
-        res.status(400).json(err);
-      }
-      let token = jwt.sign({
-        userID: req.body.userID,
-        name: req.body.userID,
-        email: req.body.userID
-      }, process.env.SECRET)
-      res.status(200).json({
-        lastLogin: user ? user.lastLogin : null,
-        token: token
-      });
+    console.log(req.headers);
+    let fb = new FB.Facebook({
+      accessToken: req.headers.accesstoken,
+      appId: process.env.FBAPPID,
+      appSecret: process.env.FBSECRET
+    });
+    fb.api(req.headers.userid, {
+      fields: ['id', 'name', 'email']
+    }, function (results) {
+      console.log(results);
+      User.findOneAndUpdate({
+        userID: req.headers.userid
+      }, {
+        lastLogin: new Date()
+      }, {
+        upsert: true,
+        setDefaultsOnInsert: true
+      }, function (err, user) {
+        if (err) {
+          console.error(err);
+          res.status(400).json(err);
+        }
+        let token = jwt.sign({
+          userID: req.headers.userid,
+          name: results.name,
+          email: results.email
+        }, process.env.SECRET)
+        res.status(200).json({
+          lastLogin: user ? user.lastLogin : null,
+          token: token
+        });
+      })
     })
   }
 }
